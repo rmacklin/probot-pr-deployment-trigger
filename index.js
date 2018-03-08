4ea4ff92acdb3e27bacb7ec65ac0a1305d5baa4f
Richard Macklin <richard.github@nrm.com>
2018-03-08 11:11:17 -0800

    Create a Deployment in GitHub when a PR is opened, reopened, or synchronized

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-module.exports = (robot) => {
-  robot.log('Yay, the app was loaded!');
+async function createGitHubDeployment(context) {
+  const octokit = context.github;
+  const payload = context.payload;
+  const pull_request = payload.pull_request;
+  const pr_branch = pull_request.head.ref;
 
-  robot.on('issues.opened', async context => {
-    robot.log('An issue was opened: ', context.payload.issue.title);
+  context.log(`A PR was ${payload.action}. Branch: `, pr_branch);
+
+  const result = await octokit.repos.createDeployment({
+    owner: payload.repository.owner.login,
+    ref: pr_branch,
+    repo: payload.repository.name,
+    auto_merge: false,
+    environment: `${payload.sender.login}-pr-${pull_request.number}`,
+    description: `Review app for '${pr_branch}' branch`,
+    transient_environment: true,
+    required_contexts: [],
+    headers: {
+      accept: 'application/vnd.github.ant-man-preview+json'
+    }
   });
+}
+
+module.exports = (robot) => {
+  robot.on(
+    ['pull_request.opened', 'pull_request.reopened', 'pull_request.synchronize'],
+    createGitHubDeployment
+  );
 };
