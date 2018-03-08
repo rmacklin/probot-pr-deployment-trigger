b5476db772efe61292addaa4dd5558068ca2a2b5
Richard Macklin <richard.github@nrm.com>
2018-03-08 11:26:56 -0800

    Deactivate stale deployments for an environment when a new deployment is made for that environment
    If we stop using `transient_environment: true` this _might_ start
    working for free...but these _are_ transient environments, so that feels
    wrong. This should work for now.

--- index.js
+++ index.js
@@ -51,6 +51,37 @@ async function removeActiveDeploymentsForClosedPR(context) {
   });
 }
 
+async function deactivateStaleDeploymentsForEnvironment(context) {
+  const octokit = context.github;
+  const payload = context.payload;
+
+  const deployments = await octokit.repos.getDeployments({
+    owner: payload.repository.owner.login,
+    // ref: payload.deployment.ref,
+    repo: payload.repository.name,
+    environment: payload.deployment.environment
+  });
+
+  // Make all deployments for this environment inactive *except* the new one
+  deployments.data.forEach(async deployment => {
+    if (deployment.id === payload.deployment.id) {
+      return;
+    }
+
+    const result = await octokit.repos.createDeploymentStatus({
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name,
+      id: deployment.id,
+      state: 'inactive',
+      headers: {
+        accept: 'application/vnd.github.ant-man-preview+json'
+      }
+    });
+
+    context.log(`added inactive deployment status to deployment ${deployment.url}`);
+  });
+}
+
 module.exports = (robot) => {
   robot.on(
     ['pull_request.opened', 'pull_request.reopened', 'pull_request.synchronize'],
@@ -61,4 +92,10 @@ module.exports = (robot) => {
     'pull_request.closed',
     removeActiveDeploymentsForClosedPR
   );
+
+  // auto_inactive didn't seem to be working (conflicts with transient_environment...??? why?)
+  robot.on(
+    'deployment',
+    deactivateStaleDeploymentsForEnvironment
+  )
 };
