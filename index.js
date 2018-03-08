567fcfa9ee9272b45ed25930bbc533dbc8c737f0
Richard Macklin <richard.github@nrm.com>
2018-03-08 11:19:35 -0800

    Remove active deployments for a PR after it is closed or merged

--- index.js
+++ index.js
@@ -21,9 +21,44 @@ async function createGitHubDeployment(context) {
   });
 }
 
+async function removeActiveDeploymentsForClosedPR(context) {
+  const octokit = context.github;
+  const payload = context.payload;
+  const merged = payload.merged;
+  const pull_request = payload.pull_request;
+  const pr_branch = pull_request.head.ref;
+
+  context.log(`A PR was ${merged ? 'merged' : 'closed'}. Branch: `, pull_request.head.ref);
+
+  const deployments = await octokit.repos.getDeployments({
+    owner: payload.repository.owner.login,
+    ref: pr_branch,
+    repo: payload.repository.name
+  });
+
+  deployments.data.forEach(async deployment => {
+    const result = await octokit.repos.createDeploymentStatus({
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name,
+      id: deployment.id,
+      state: 'inactive',
+      headers: {
+        accept: 'application/vnd.github.ant-man-preview+json'
+      }
+    });
+
+    context.log(`added inactive deployment status to deployment ${deployment.url}`);
+  });
+}
+
 module.exports = (robot) => {
   robot.on(
     ['pull_request.opened', 'pull_request.reopened', 'pull_request.synchronize'],
     createGitHubDeployment
+  );
+
+  robot.on(
+    'pull_request.closed',
+    removeActiveDeploymentsForClosedPR
   );
 };
