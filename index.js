199135c05dc1f0abb676a34f0b05f5c7a576bea4
Richard Macklin <richard.github@nrm.com>
2018-03-08 10:51:06 -0800

    Add example webhook event responder

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 module.exports = (robot) => {
-  // Your code here
   robot.log('Yay, the app was loaded!');
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  robot.on('issues.opened', async context => {
+    robot.log('An issue was opened: ', context.payload.issue.title);
+  });
 };
