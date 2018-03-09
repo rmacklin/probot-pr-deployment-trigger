082be45e2d95f68c677e4f3ba612f4251b0b85d8
Richard Macklin <richard.github@nrm.com>
2018-03-09 03:02:50 -0800

    Use PR author, not event sender

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ async function createGitHubDeployment(context) {
     ref: pr_branch,
     repo: payload.repository.name,
     auto_merge: false,
-    environment: `${payload.sender.login}-pr-${pull_request.number}`,
+    environment: `${pull_request.user.login}-pr-${pull_request.number}`,
     description: `Review app for '${pr_branch}' branch`,
     transient_environment: true,
     required_contexts: [],
